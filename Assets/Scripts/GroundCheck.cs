b01cea32a8004650581abe5383497322ba5a9177
nicloay <nicloay@gmail.com>
2015-05-25 10:51:31 +0300

    initial commit

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+	public LayerMask	GroundLayers;
+	public Collider2D 	GroundCheckCollider;
+	public bool IsGrounded{
+		get{
+			return _isGrounded;
+		}
+	}
+
+	bool _isGrounded;
+
+	void FixedUpdate () {
+		_isGrounded = GroundCheckCollider.IsTouchingLayers(GroundLayers.value);
+	}
+#if UNITY_EDITOR
+	void OnDrawGizmos(){
+		UnityEditor.Handles.BeginGUI();
+		GUI.Label(new Rect(10,10,150,20), "isGrounded: " + IsGrounded);
+		UnityEditor.Handles.EndGUI();	
+	}
+#endif
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementController : MonoBehaviour {
+	public float HorizontalForce = 1000.0f;
+	public float JumpForce = 500.0f;
+	Rigidbody2D rBoby;
+
+	void Start(){
+		rBoby = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		float x = Input.GetAxis("Horizontal");
+		float y = 0.0f;
+		if (Input.GetButtonDown("Jump")){
+			y = JumpForce;
+		}
+		rBoby.AddForce(new Vector2(x*HorizontalForce*Time.deltaTime, y));
+	}
+}
